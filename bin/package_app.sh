bf074a99d4868fd3e21a0c4d5571704540f3554d
zarlex <alex.zarges@online.de>
2017-12-10 23:23:59 +0100

    Install node

--- bin/package_app.sh
+++ bin/package_app.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env bash
 set -e
 
-npm install -g electron-packager
 brew update
 brew upgrade
+brew install node
 brew install Caskroom/cask/xquartz
 brew install wine
 brew install mono
 brew install gnu-tar graphicsmagick xz
 brew install rpm
+
+npm install -g electron-packager
+npm install
 npm run dist:all
