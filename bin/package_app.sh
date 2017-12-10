cdc05ef7ad1df47f6acc3820c61caaa370bd6732
zarlex <alex.zarges@online.de>
2017-12-11 00:13:14 +0100

    Move installation into before_deploy

--- bin/package_app.sh
+++ bin/package_app.sh
@@ -1,15 +1,4 @@
 #!/usr/bin/env bash
 set -e
 
-brew update
-brew upgrade
-brew install node
-brew install Caskroom/cask/xquartz
-brew install wine
-brew install mono
-brew install gnu-tar graphicsmagick xz
-brew install rpm
-
-npm install -g electron-packager
-npm install
 npm run dist:all
