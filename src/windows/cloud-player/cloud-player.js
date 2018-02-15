a149cfb68bec1068d0c44efc2b016aa8df421c77
zarlex <alex.zarges@online.de>
2018-02-15 17:25:20 +0100

    Restore window in the last size and last pos before showing it

--- src/windows/cloud-player/cloud-player.js
+++ src/windows/cloud-player/cloud-player.js
@@ -25,41 +25,59 @@ var PortalWindow = Window.extend({
       preload: __dirname + '/preload.js'
     }
   },
-  showScConnectWindow: function (url) {
-    this._connectionWindow = new ConnectWindow({url: url});
+  windowsProperties: new WindowsProperties(),
+  windowProperties: null,
+  showConnectWindow: function (url) {
+    this._connectionWindow = new ConnectWindow({
+      url: url,
+      options: {
+        parent: this.window,
+        modal: false
+      }
+    });
     this._connectionWindow.open();
   },
-  initialize: function () {
-    var windowsProperties = new WindowsProperties(),
-      size = electron.screen.getPrimaryDisplay().workAreaSize,
-      windowProperties;
+  setLastWindowSize: function () {
+    var dfd = Q.defer(),
+      size = electron.screen.getPrimaryDisplay().workAreaSize;
 
     this.window.setContentSize(size.width, size.height);
 
-    windowsProperties.find({name: 'portal'}).then(function (collection) {
+    this.windowsProperties.find({name: 'portal'}).then(function (collection) {
       if (collection.length === 0) {
-        windowProperties = new WindowProperties({name: 'portal'});
+        this.windowProperties = new WindowProperties({name: 'portal'});
       } else {
-        windowProperties = collection.first();
-        var size = windowProperties.get('size'),
-          position = windowProperties.get('position');
+        this.windowProperties = collection.first();
+        var size = this.windowProperties.get('size'),
+          position = this.windowProperties.get('position');
 
         this.window.setContentSize(size.width, size.height);
         this.window.setPosition(position.x, position.y);
       }
+      dfd.resolve();
     }.bind(this));
 
+    return dfd.promise;
+  },
+  beforeShow: function () {
+    var dfd = Q.defer();
+    this.setLastWindowSize().then(function () {
+      dfd.resolve();
+    });
+    return dfd.promise;
+  },
+  initialize: function () {
     this.on('close', function () {
-      if (windowProperties) {
-        windowProperties.set('size', {
+      if (this.windowProperties) {
+        this.windowProperties.set('size', {
           width: this.window.getContentSize()[0],
           height: this.window.getContentSize()[1]
         });
-        windowProperties.set('position', {
+        this.windowProperties.set('position', {
           x: this.window.getPosition()[0],
           y: this.window.getPosition()[1]
         });
-        windowProperties.save();
+        this.windowProperties.save();
       }
     }.bind(this));
 
